85a49ee47998305ecef702b441180629214e5678
keinberger <philipp.keinberger@gmail.com>
2025-09-18 15:46:08 +0300

    chore: update contract deployment address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const NODE_URL = "https://rpc.testnet.miden.io:443";
-export const TIC_TAC_TOE_CONTRACT_ID = "0x5b0b8a4e61d209400c7836fc414653";
+export const TIC_TAC_TOE_CONTRACT_ID = "0xb8847bf76d3ce64006fcaa4be8c5eb";
 
 export const NONCE_SLOT = 0;
 export const PLAYER_IDS_MAPPING_SLOT = 1;
