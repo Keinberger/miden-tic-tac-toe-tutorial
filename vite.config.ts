62be16527a686996409de731f36f00195063c69e
keinberger <philipp.keinberger@gmail.com>
2025-09-18 16:49:30 +0300

    chore: make build

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,15 @@ export default defineConfig({
     exclude: ["@demox-labs/miden-sdk"],
     include: ["buffer"],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
+    },
+  },
+  worker: {
+    format: "es",
+  },
   assetsInclude: ["**/*.masm"], // Include .masm files as assets
 });
